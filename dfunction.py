b6b161ee0d7ec4c7df614fe05a89684391312e5f
Shreeram <sprajapati99.sp@gmail.com>
2025-02-08 14:46:27 +0530

    2nd Commit & modified forloop

--- dfunction.py
+++ dfunction.py
@@ -1,3 +1,5 @@
 def double(n):
     return 2*n
 print("Hello ShreerAm")
+print("Modified function")
+print("Added to see checkout for see chAges")
---
+++ forloop.py
@@ -0,0 +1,2 @@
+print("Added to see checkout for see chAges")
+print("Hello 2nd commit")
